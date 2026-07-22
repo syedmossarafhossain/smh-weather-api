5b7179a0f325f4efedaa6c6e613c38eec82e007a
Syed Mossaraf Hossain <syedmossarafhossain@gmail.com>
2026-07-22 18:02:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ const notFoundSection = document.querySelector('.not-found');
 const searchCitySection = document.querySelector('.search-city');
 
 const apiKey = '0f165508335f879c5c06c610efe253c9';
+
+// BUTTON CLICK
+searchBtn.addEventListener('click', () =>{
+    if (cityInput.value.trim() != ''){
+        updateWeatherInfo(cityInput.value);
+        cityInput.value = '';
+        cityInput.blur();
+    }
+})
+
+
+// SEARCH INPUT
+cityInput.addEventListener('keydown', (event) =>{
+    if (event.key === 'Enter' && cityInput.value.trim() != ''){
+        updateWeatherInfo(cityInput.value);
+        cityInput.value = '';
+        cityInput.blur();
+    }
+    
+})
+
+
+// FETCH DATA
+async function getFetchData(endPoint, city){
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`;
+
+    const response = await fetch(apiUrl);
+
+    return response.json();
+}
