4c6e0f6dbf99a7bfbeab5e460bd70070eeb4e7ea
Syed Mossaraf Hossain <syedmossarafhossain@gmail.com>
2026-07-22 17:59:51 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const cityInput = document.querySelector('.city-input');
+const searchBtn = document.querySelector('.search-btn');
+
+
+const weatherInfoSection = document.querySelector('.weather-info');
+const notFoundSection = document.querySelector('.not-found');
+const searchCitySection = document.querySelector('.search-city');
+
+const apiKey = '0f165508335f879c5c06c610efe253c9';
