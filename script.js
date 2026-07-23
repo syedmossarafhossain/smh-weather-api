f19c27de7a6df3a04059e721edeacdf47c65fa86
Syed Mossaraf Hossain <syedmossarafhossain@gmail.com>
2026-07-23 18:43:50 +0530

    Refactor weather script to include additional elements

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ const weatherInfoSection = document.querySelector('.weather-info');
 const notFoundSection = document.querySelector('.not-found');
 const searchCitySection = document.querySelector('.search-city');
 
+const countryTxt = document.querySelector('.country-txt');
+const tempTxt = document.querySelector('.temp-txt');
+const conditionTxt = document.querySelector('.condition-txt');
+const humidityValueTxt = document.querySelector('.humidity-value-txt');
+const windValueTxt = document.querySelector('.wind-value-txt');
+const weatherSummaryImg = document.querySelector('.weather-summary-img');
+const currentDateTxt = document.querySelector('.current-date-txt');
+
 const apiKey = '0f165508335f879c5c06c610efe253c9';
 
 // BUTTON CLICK
